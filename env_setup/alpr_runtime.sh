dd7dbad64de02b4b76065b5cde77510a70b08ff7
tonywang-github <61956751+tonywang-github@users.noreply.github.com>
2022-02-07 17:21:33 +0800

    Update alpr_runtime.sh

--- env_setup/alpr_runtime.sh
+++ env_setup/alpr_runtime.sh
@@ -6,8 +6,8 @@ sudo apt -y install libqtgui4
 sudo apt -y install libqt4-test
 sudo pip3 install --upgrade pip
 sudo pip3 install wheel setuptools pySerial requests
-sudo pip3 install /home/linaro/Desktop/Demo_ALPR_EdgeR_v1.0.14_20220207/numpy-1.18.5-cp35-cp35m-linux_aarch64.whl
-sudo pip3 install /home/linaro/Desktop/Demo_ALPR_EdgeR_v1.0.14_20220207/opencv_python-4.1.1.26-cp35-cp35m-linux_aarch64.whl
+sudo pip3 install numpy-1.18.5-cp35-cp35m-linux_aarch64.whl
+sudo pip3 install opencv_python-4.1.1.26-cp35-cp35m-linux_aarch64.whl
 echo "********Install Docker********"
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
